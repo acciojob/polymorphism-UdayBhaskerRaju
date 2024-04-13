9e3011246d4dda654df166a2ec9a1bd6ea71233c
Uday Bhasker Raju <udaybhaskerraju@example.com>
2024-04-13 10:28:55 -0700

    added main functon

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,10 +1,13 @@
 package com.driver;
 
 public class Main {
-    Product p = new Product();
-    int ans = p.product(2,3);
-    int ans1 = p.product(2,3,4);
-    double ans2 = p.product(2.0,3.0);
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        int ans = p.product(2,3);
+        int ans1 = p.product(2,3,4);
+        double ans2 = p.product(2.0,3.0);
+    }
     public static class Product{
          public int product(int x, int y) {
              return x+y;
