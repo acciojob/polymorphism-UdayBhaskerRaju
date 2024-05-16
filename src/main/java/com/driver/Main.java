68365992a575b4c5404e4a46692cdf771631caca
Uday Bhasker Raju <udaybhaskerraju@example.com>
2024-05-16 10:49:48 -0700

    commit 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -17,6 +17,6 @@ public class Main {
         Product p = new Product();
         int ans = p.product(2,3);
         int ans1 = p.product(2,3,4);
-        double ans2 = p.product(7,2);
+        double ans2 = p.product(7.2,2.5);
     }
 }
