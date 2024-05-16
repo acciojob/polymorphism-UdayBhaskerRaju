dcb34a049837040a7d2972af1a2b41dba32b0051
Uday Bhasker Raju <udaybhaskerraju@example.com>
2024-05-16 10:45:21 -0700

    polymorphism assignment submision

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,21 +2,21 @@ package com.driver;
 
 public class Main {
 
+    public static class Product{
+        public int product(int x, int y) {
+            return x+y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x/y;
+        }
+    }
     public static void main(String[] args) {
         Product p = new Product();
         int ans = p.product(2,3);
         int ans1 = p.product(2,3,4);
-        double ans2 = p.product(2.0,3.0);
-    }
-    public static class Product{
-         public int product(int x, int y) {
-             return x+y;
-         }
-        public int product(int x, int y, int z) {
-             return x*y*z;
-        }
-        public double product(double x, double y) {
-             return x+y;
-        }
+        double ans2 = p.product(7,2);
     }
 }
